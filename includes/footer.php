9a653707139c0cdf78ac3c2b984f8dc34c8c6ed7
TraseRRR98 <traserrr@gmail.com>
2024-07-01 23:04:05 -0400

    chore: Update styles and scripts for header, navbar, and footer

--- includes/footer.php
+++ includes/footer.php
@@ -1,5 +1,5 @@
-<footer>
-    <div class="container">
+<footer class="bg-light text-center text-lg-start">
+    <div class="container p-4">
         <p>&copy; 2024 Workout Tracker. All rights reserved.</p>
     </div>
 </footer>
--- index.php
+++ index.php
@@ -5,14 +5,20 @@ include 'includes/navbar.php';
 ?>
 
 <div class="container">
-    <h1>Welcome to Workout Tracker</h1>
-    <p>Track your workouts and progress with ease!</p>
+    <div class="jumbotron mt-4">
+        <h1 class="display-4">Welcome to Workout Tracker</h1>
+        <p class="lead">Track your workouts and progress with ease!</p>
+        <hr class="my-4">
+        <p>Get started by logging your exercises and creating workout plans.</p>
+    </div>
 </div>
 
 <?php
 include 'includes/footer.php';
 ?>
 
-<script src="public/js/bootstrap.bundle.min.js"></script>
+<script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+<script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.5.4/dist/umd/popper.min.js"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.5.2/js/bootstrap.min.js"></script>
 </body>
 </html>
