2a72a9fa25c0948529fd15de956eaf5e9ecffd53
TraseRRR98 <traserrr@gmail.com>
2024-07-01 23:00:23 -0400

    chore: Add database connection script, header, navbar, and footer files

---
+++ index.php
@@ -0,0 +1,18 @@
+<?php
+include 'includes/db_connect.php';
+include 'includes/header.php';
+include 'includes/navbar.php';
+?>
+
+<div class="container">
+    <h1>Welcome to Workout Tracker</h1>
+    <p>Track your workouts and progress with ease!</p>
+</div>
+
+<?php
+include 'includes/footer.php';
+?>
+
+<script src="public/js/bootstrap.bundle.min.js"></script>
+</body>
+</html>
